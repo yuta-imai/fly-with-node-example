2d941a490a5b51306d05f3e0fdb214053961a03a
Yuta Imai <imai.factory@gmail.com>
2021-11-04 17:47:27 +0900

    add !!!!!!!!

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get(["/", "/:name"], (req, res) => {
-  greeting = "<h1>Hello From Node on Fly!</h1>";
+  greeting = "<h1>Hello From Node on Fly!!!!!!!!!!!!!!</h1>";
   name = req.params["name"];
   if (name) {
     res.send(greeting + "</br>and hello to " + name);
